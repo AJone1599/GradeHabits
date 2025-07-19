7588fb199ebe4aeab2b5f6bf5c405fe9d6f013f9
Alexis Justin Ballesteros <aj.one1599@gmail.com>
2025-07-19 11:24:53 -0400

    Machine learning training

--- BackEnd/backend.py
+++ BackEnd/backend.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+import joblib
+model = joblib.load('/Machine Learning/gradePredictionModel.pkl')
 
 app = Flask(__name__)
 CORS(app)
 @app.route('/api/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    features = data.get('features', [0, 0, 0])
+    '''features = data.get('features', [0, 0, 0])
     # Dummy prediction logic: sum the features
     print(features)
-    prediction = sum(features)
+    prediction = sum(features)'''
+    answers = data.get('answers', [])
+    print(answers)
+    #Change answers to a format suitable for the model
+    prediction = model.predict([answers])
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
--- Machine Learning/GradePrediction.py
+++ Machine Learning/GradePrediction.py
@@ -1,2 +1,28 @@
 from lightgbm import LGBMRegressor
 import shap
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import joblib
+
+train_path = 'Machine Learning/train.csv'
+test_path = 'Machine Learning/test.csv'
+
+def load_data():
+    train_data = pd.read_csv(train_path)
+    test_data = pd.read_csv(test_path)
+    return train_data, test_data
+train_data, test_data = load_data()
+X = train_data.drop(columns=['gpa'])
+y = train_data['gpa']
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LGBMRegressor()
+model.fit(train_X, train_y)
+predictions = model.predict(val_X)
+val_mae = mean_absolute_error(predictions, val_y)
+print("Validation MAE: {:,.0f}".format(val_mae))
+
+explainer = shap.Explainer(model, train_X)
+shap_values = explainer(val_X)
+joblib.dump(model, '/Machine Learning/gradePredictionModel.pkl')
