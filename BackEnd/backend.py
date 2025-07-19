b2ba69f66fb6594942e87d1cf1f61444fa8528d1
Alexis Justin Ballesteros <aj.one1599@gmail.com>
2025-07-19 11:11:31 -0400

    Update

--- BackEnd/backend.py
+++ BackEnd/backend.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder='Front End')
+app = Flask(__name__)
 CORS(app)
 @app.route('/api/predict', methods=['POST'])
 def predict():
@@ -11,18 +11,6 @@ def predict():
     print(features)
     prediction = sum(features)
     return jsonify({'prediction': prediction})
-
-@app.route('/')
-def home_page():
-    return render_template('homepage.html')
-
-@app.route('/survey')
-def survey_page():
-    return render_template('survey.html')
-
-@app.route('/results')
-def results_page():
-    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
